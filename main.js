6d5a76895aeda86b552fc9d214b21fc35e6b7a05
ermuxanov <74331498+ermuxanov@users.noreply.github.com>
2022-10-08 15:08:32 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+var time = +prompt("Введите время в цифрах");
+
+switch (time) {
+  case 1:
+    alert("Час ночи");
+    break;
+  case 2:
+    alert(time + " часа ночи");
+    break;
+  case 3:
+    alert(time + " часа ночи");
+    break;
+  case 4:
+    alert(time + " часа ночи");
+    break;
+  case 5:
+    alert(time + " часа ночи");
+    break;
+  case 6:
+    alert(time + " часов утра");
+    break;
+  case 7:
+    alert(time + " часов утра");
+    break;
+  case 8:
+    alert(time + " часов утра");
+    break;
+  case 9:
+    alert(time + " часов утра");
+    break;
+  case 10:
+    alert(time + " часов утра");
+    break;
+  case 11:
+    alert(time + " часов утра");
+    break;
+  case 12:
+    alert("Полдень");
+    break;
+  case 13:
+    alert("Час дня");
+    break;
+  case 14:
+    alert(time - 12 + " часа дня");
+    break;
+  case 15:
+    alert(time - 12 + " часа дня");
+    break;
+  case 16:
+    alert(time - 12 + " часа дня");
+    break;
+  case 17:
+    alert(time - 12 + " часа дня");
+    break;
+  case 18:
+    alert(time - 12 + " часа дня");
+    break;
+  case 19:
+    alert(time - 12 + " часов вечера");
+    break;
+  case 20:
+    alert(time - 12 + " часов вечера");
+    break;
+  case 21:
+    alert(time - 12 + " часов вечера");
+    break;
+  case 22:
+    alert(time - 12 + " часов вечера");
+    break;
+  case 23:
+    alert(time - 12 + " часов вечера");
+    break;
+  case 24:
+    alert("Полночь");
+    break;
+  default:
+    alert("такого дня нету");
+}
